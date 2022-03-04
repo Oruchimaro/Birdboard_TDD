6175958f4d0f3ba81ee99d5e524b9ef38b98c6cb
Amir Daneshkar <amir.daneshkar@hotmail.com>
2022-03-04 22:48:05 +0330

    Install Framework, set up Laravel UI, set up testing

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ExampleTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_the_application_returns_a_successful_response()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_database_in_memory_works_fine()
+    {
+        $response = User::factory()->create();
+
+        $this->assertDatabaseCount('users',1);
+    }
+}
